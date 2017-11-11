57ba2885e162bbf554a60553ea8c4cb682c825d7
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-11 15:30:09 -0800

    Cleanup (to my taste) project structure and code, get build to work - Refactor Makefile, make gcc called explicitly, make code -fPIC
    Cleanup formatting (to my taste)

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-lisp500 : lisp500.c
-	cc  -o lisp500 -lm  lisp500.c
+lisp500 : lisp500.o
+	gcc -o lisp500 lisp500.o -lm -ldl
+#	cc  -o lisp500 -lm  lisp500.c
 
-# lisp500.o : lisp500.c
-# 	cc -c lisp500.c
+lisp500.o : lisp500.c
+	cc -c lisp500.c
 
 clean :
-	rm lisp500 lisp500.o
+	rm -f lisp500 lisp500.o
--- lisp500.c
+++ lisp500.c
@@ -150,58 +150,60 @@ st:	t = (lval *) (v & ~3);
 		}
 	}
 }
-lval gc(lval * f)
-{
-	int i;
-	lval *m;
-	int l;
-	int u = 0;
-	int ml;
-	printf(";garbage collecting...\n");
-	while (memf) {
-		lval *n = (lval *) memf[0];
-		memset(memf, 0, 4 * memf[1]);
-		memf = (lval *) n;
-	}
-	gcm(xvalues);
-	gcm(pkgs);
-	gcm(dyns);
-	for (; f > stack; f--) {
-		if ((*f & 3) && (*f < memory || *f > memory + memory_size / 4))
-			printf("%x\n", *f);
-		gcm(*f);
-	}
-	memf = 0;
-	m = memory;
-	i = 0;
-	while (m < memory + memory_size / 4) {
-		l = ((m[1] & 4 ? m[0] >> 8 : 0) + 1) & ~1;
-		if (m[0] & 4) {
-			if (u) {
-				m[-ml] = (lval) memf;
-				m[1 - ml] = ml;
-				memf = m - ml;
-				u = 0;
-				i += ml;
-			}
-		} else {
-			if (!u)
-				ml = 0;
-			ml += l + 2;
-			u = 1;
-		}
-		m[0] &= ~4;
-		m += l + 2;
-	}
-	if (u) {
-		m[-ml] = (lval) memf;
-		m[1 - ml] = ml;
-		memf = m - ml;
-		i += ml;
-	}
-	printf(";done. %d free.\n", i);
-	return 0;
+
+lval gc(lval* f) {
+  int i;
+  lval *m;
+  int l;
+  int u = 0;
+  int ml;
+  printf(";garbage collecting...\n");
+  while (memf) {
+    lval *n = (lval *) memf[0];
+    memset(memf, 0, 4 * memf[1]);
+    memf = (lval *) n;
+  }
+  gcm(xvalues);
+  gcm(pkgs);
+  gcm(dyns);
+  for (; f > stack; f--) {
+    if ((*f & 3) && (*f < memory || *f > memory + memory_size / 4)) {
+      printf("%x\n", *f);
+    }
+    gcm(*f);
+  }
+  memf = 0;
+  m = memory;
+  i = 0;
+  while (m < memory + memory_size / 4) {
+    l = ((m[1] & 4 ? m[0] >> 8 : 0) + 1) & ~1;
+    if (m[0] & 4) {
+      if (u) {
+	m[-ml] = (lval) memf;
+	m[1 - ml] = ml;
+	memf = m - ml;
+	u = 0;
+	i += ml;
+      }
+    } else {
+      if (!u)
+	ml = 0;
+      ml += l + 2;
+      u = 1;
+    }
+    m[0] &= ~4;
+    m += l + 2;
+  }
+  if (u) {
+    m[-ml] = (lval) memf;
+    m[1 - ml] = ml;
+    memf = m - ml;
+    i += ml;
+  }
+  printf(";done. %d free.\n", i);
+  return 0;
 }
+
 lval *m0(lval * g, int n)
 {
 	lval *m = memf;
