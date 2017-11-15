10829ff239f0ad5fc83d59167ac2ba2711d77a56
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-15 00:09:58 -0800

    Add testing instrumentation

--- lisp500.c
+++ lisp500.c
@@ -1091,7 +1091,7 @@ lval liref(lval* f) {
     write(1, "out of bounds in iref\n", 22);
   }
   lval* tmp_lval =  lisp_word_to_c_pointer((f[1] & ~3));
-  return ((lval *) (tmp_lval))[o2u(f[2])] & ~4;
+  return (tmp_lval)[o2u(f[2])] & ~4;
   // return ((lval *) (f[1] & ~3))[o2u(f[2])] & ~4;
 }
 
