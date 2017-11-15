cd067046e879bba80da8db8b2dbb6950a4effeba
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-15 00:05:41 -0800

    Add testing instrumentation

--- lisp500.c
+++ lisp500.c
@@ -1090,7 +1090,9 @@ lval liref(lval* f) {
   if (o2u(f[2]) >= o2a(f[1])[0] / 256 + 2) {
     write(1, "out of bounds in iref\n", 22);
   }
-  return ((lval *) (f[1] & ~3))[o2u(f[2])] & ~4;
+  lval tmp_lval =  (f[1] & ~3);
+  return ((lval *) (tmp_lval))[o2u(f[2])] & ~4;
+  // return ((lval *) (f[1] & ~3))[o2u(f[2])] & ~4;
 }
 
 lval setfiref(lval * f) {
