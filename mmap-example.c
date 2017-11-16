b68dda7a4463f4ce04e9fa1439dba84cc6e0c218
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-16 13:24:06 -0800

    Add testing instrumentation

--- mmap-example.c
+++ mmap-example.c
@@ -15,19 +15,20 @@ typedef struct
 } myStruct;
 
 // must be at least 32bits
-uintptr_t mem_region_a_suggested = 0x80000000;
-uintptr_t mem_region_a = 0x80000000;
+uintptr_t suggested_mem_region_a = 0x80000000;
+uintptr_t mem_region_a = 0;
 
-int alocate_memory() {
-  mem_region_a =
-    (uintptr_t)mmap((void*)mem_region_a_suggested, 1024 * sizeof(lval),
+int alocate_memory(void* suggested_mem_location) {
+  uintptr_t mem_location =
+    (uintptr_t)mmap(suggested_mem_location, 1024 * sizeof(lval),
 		    PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-  if ((void*)mem_region_a == MAP_FAILED) {
+  if ((void*)mem_location == MAP_FAILED) {
     printf("Map failed.\n");
   } else {
-    if (mem_region_a != mem_region_a_suggested) {
-      printf("Allocated memory had to be shifted: %li .\n", (long)mem_region_a);
+    if (mem_location != suggested_mem_location) {
+      printf("Allocated memory had to be shifted: %li .\n",
+	     (long)mem_region_a);
     }
     lval* entry = (lval*)mem_region_a;
     entry[0] = 4;
@@ -36,6 +37,6 @@ int alocate_memory() {
 }
 
 int main() {
-  alocate_memory();
+  mem_region_a = (uintptr_t)alocate_memory((void*)suggested_mem_location);
   return 0;
 }
