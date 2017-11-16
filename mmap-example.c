6ddaa0a13d9fcebfa016b5b0d343e5dff17456bd
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-16 13:19:28 -0800

    Add testing instrumentation

--- mmap-example.c
+++ mmap-example.c
@@ -3,24 +3,39 @@
 
 #include <sys/mman.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdint.h>
+
+#include "defs.h"
 
 typedef struct
 {
-    int a;
-    int b;
+  int a;
+  int b;
 } myStruct;
 
-int main()
-{
-    myStruct * entry = (myStruct *)mmap(0, sizeof(myStruct),
-            PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+// must be at least 32bits
+uintptr_t mem_region_a_suggested = 0x80000000;
+uintptr_t mem_region_a = 0x80000000;
 
-    if (entry == MAP_FAILED) {
-        printf("Map failed.\n");
+int alocate_memory() {
+  mem_region_a =
+    (uintptr_t)mmap((void*)mem_region_a_suggested, 1024 * sizeof(lval),
+		    PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+  if ((void*)mem_region_a == MAP_FAILED) {
+    printf("Map failed.\n");
+  } else {
+    if (mem_region_a != mem_region_a_suggested) {
+      printf("Allocated memory had to be shifted: %li .\n", (long)mem_region_a);
     }
-    else {
-        entry->a = 4;
-        printf("Success: entry=%p, entry->a = %d\n", entry, entry->a);
-    }
-    return 0;
+    lval* entry = (lval*)mem_region_a;
+    entry[0] = 4;
+    printf("Success: entry=%p, entry->a = %d\n", entry, entry[0]);
+  }
+}
+
+int main() {
+  alocate_memory();
+  return 0;
 }
