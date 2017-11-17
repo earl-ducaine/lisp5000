5df3348013b2377b8b37f21c3159512c97b697f3
Earl DuCaine <earl.ducaine-gmail-com>
2017-11-16 16:18:02 -0800

    Add testing instrumentation

--- memory.c
+++ memory.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdint.h>
+#include <error.h>
 
 #include "defs.h"
 
@@ -12,19 +13,42 @@
 uintptr_t SUGGESTED_MEM_LOCATION_REGION_A = 0x80000000;
 uintptr_t mem_region_a = 0;
 
-void* alocate_memory(void* suggested_mem_location, uintptr_t size_in_bytes) {
+
+
+// malloc based memory allocation
+void* alocate_region_malloc(long size_in_bytes) {
+  void* mem_location = malloc(size_in_bytes);
+  if (!mem_location) {
+    int error_save = errno;
+    printf("Map failed with error code: %i\n", error_save);
+  }
+  return mem_location;
+}
+
+// malloc based memory allocation
+void* alocate_memory_mmap(void* suggested_mem_location, uintptr_t size_in_bytes) {
   void* mem_location =
     (void*)mmap(suggested_mem_location, size_in_bytes * sizeof(lval),
 		    PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (mem_location == MAP_FAILED) {
-    printf("Map failed.\n");
+    int error_save = errno;
+    printf("Map failed with error code: %i\n", error_save);
   } else {
     if (mem_location != suggested_mem_location) {
       printf("Allocated memory had to be shifted: %li .\n",
 	     (long)mem_location);
     }
-    lval* entry = (lval*)mem_location;
-    entry[0] = 4;
-    printf("Success: entry=%p, entry->a = %d\n", entry, entry[0]);
   }
+  return mem_location;
 }
+
+
+
+ /* if (somecall() == -1) { */
+ /*               int errsv = errno; */
+ /*               printf("somecall() failed\n"); */
+ /*               if (errsv == ...) { ... } */
+ /*           } */
+ /*    lval* entry = (lval*)mem_location; */
+ /*    entry[0] = 4; */
+ /*    printf("Success: entry=%p, entry->a = %d\n", entry, entry[0]); */
